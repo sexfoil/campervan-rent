53dfee31a62e205492b45acd57b5a023579df61d
Slava Poliakov <78slavapolyakov@gmail.com>
2024-04-17 09:01:42 +0300

    Fixed homepage

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/campervan-rent',
+  base: '/campervan-rent/',
   resolve: {
     alias: {
       properties: ['/src/properties'],
