a53db9145e1cae808263f55d38314c9870b51f60
Slava Poliakov <78slavapolyakov@gmail.com>
2024-04-17 20:25:47 +0300

    Edited configuration

--- novite.config.js
+++ novite.config.js
@@ -1,17 +1,17 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/campervan-rent/',
-//   resolve: {
-//     alias: {
-//       properties: ['/src/properties'],
-//       components: ['/src/components'],
-//       pages: ['/src/pages'],
-//       api: ['/src/api'],
-//       svg: ['/src/svg'],
-//     },
-//   },
-// });
+export default defineConfig({
+  plugins: [react()],
+  base: '/campervan-rent/',
+  resolve: {
+    alias: {
+      properties: ['/src/properties'],
+      components: ['/src/components'],
+      pages: ['/src/pages'],
+      api: ['/src/api'],
+      svg: ['/src/svg'],
+    },
+  },
+});
