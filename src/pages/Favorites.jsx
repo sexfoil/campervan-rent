421e9bae078f452d1222561fbb4026b4bee907b7
Slava Poliakov <78slavapolyakov@gmail.com>
2024-04-24 22:26:30 +0300

    Add modal for favorites

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,33 +1,36 @@
-import SvgIcon from 'components/SvgIcon/SvgIcon';
 import { useSelector } from 'react-redux';
-import { selectFavorites } from 'store/selector';
+import { selectCurrentCamper, selectFavorites } from 'store/selector';
 import CamperInfoCard from 'components/Camper/CamperInfoCard/CamperInfoCard';
+import ModalCamperItem from 'components/Modal/ModalCamperItem/ModalCamperItem';
 import { useScrollVisibility } from 'hooks/useScrollVisibility';
+import ScrollUpButton from 'components/Button/ScrollUpButton';
 import css from './Favorites.module.css';
 
 const Favorites = () => {
   const favorites = useSelector(selectFavorites);
+  const camper = useSelector(selectCurrentCamper);
   const scrollUp = useScrollVisibility(false);
 
   const onClickScrollUp = () => window.scrollTo({ top: 0, behavior: 'smooth' });
 
   return (
-    <div className={css.container}>
-      {favorites.length !== 0 ? (
-        <ul className={css.list}>
-          {favorites.map(camper => {
-            return <CamperInfoCard key={camper._id} camper={camper} />;
-          })}
-        </ul>
-      ) : (
-        'It seems you have no favorites yet'
-      )}
-      {scrollUp.isVisible && (
-        <button className={css.scroll} type="button" onClick={onClickScrollUp}>
-          <SvgIcon icon={'scrollup'} />
-        </button>
-      )}
-    </div>
+    <>
+      <div className={css.container}>
+        {favorites.length !== 0 ? (
+          <ul className={css.list}>
+            {favorites.map(camper => {
+              return <CamperInfoCard key={camper._id} camper={camper} />;
+            })}
+          </ul>
+        ) : (
+          'It seems you have no favorites yet'
+        )}
+        {scrollUp.isVisible && (
+          <ScrollUpButton onClickScrollUp={onClickScrollUp} />
+        )}
+      </div>
+      {camper && <ModalCamperItem />}
+    </>
   );
 };
 
