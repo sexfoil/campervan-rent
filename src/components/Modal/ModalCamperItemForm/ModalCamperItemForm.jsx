1ad52f73f4046a7587283f09693fbbb766420d72
Slava Poliakov <78slavapolyakov@gmail.com>
2024-04-25 09:37:35 +0300

    Add modal booking form

--- src/components/Modal/ModalCamperItemForm/ModalCamperItemForm.jsx
+++ src/components/Modal/ModalCamperItemForm/ModalCamperItemForm.jsx
@@ -1,7 +1,23 @@
+import SubmitButton from 'components/Button/SubmitButton';
 import css from './ModalCamperItemForm.module.css';
+import { NAMES } from 'properties/Constants';
 
 export const ModalCamperItemForm = () => {
-  return <div className={css.container}>FORM</div>;
+  return (
+    <form className={css.container}>
+      <div className={css.header}>
+        <h2>Book your campervan now</h2>
+        <p>Stay connected! We are always ready to help you.</p>
+      </div>
+      <div className={css.fields}>
+        <input className={css.input} type="text" placeholder="Name" />
+        <input className={css.input} type="email" placeholder="Email" />
+        <input className={css.input} type="date" placeholder="Booking date" />
+        <input className={css.input} type="text" placeholder="Comment" />
+      </div>
+      <SubmitButton name={NAMES.BUTTONS.send} />
+    </form>
+  );
 };
 
 export default ModalCamperItemForm;
